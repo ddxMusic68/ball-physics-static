9ee00f4f6d1b841eb33c721d43294ac701fb3d3b
Jackson Ramig <superhot3032@gmail.com>
2026-01-05 11:00:26 -0600

    added html and some js

---
+++ main.js
@@ -0,0 +1,13 @@
+class Ball {
+    constructor(radius=5, gravity=2, x_vel=0, y_vel=0, x=0, y=0) {
+        this.radius = radius
+        this.gravity = gravity
+        this.x_vel = x_vel
+        this.y_vel = y_vel
+        this.x = x
+        this.y = y
+    }
+}
+
+const ball = document.createElement('div')
+document.addElement()
